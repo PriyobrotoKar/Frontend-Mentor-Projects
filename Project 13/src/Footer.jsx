4a8fd71fe1307c584c8b50642750f2ed66addb26
Priyobroto Kar <priyobrotokar@gmail.com>
2023-05-18 23:32:23 +0530

    Adde the footer tag

--- Project 13/src/Footer.jsx
+++ Project 13/src/Footer.jsx
@@ -2,13 +2,13 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="attribution absolute bottom-0 w-full text-center">
+    <footer className="attribution absolute bottom-0 w-full text-center">
       Challenge by{" "}
       <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
         Frontend Mentor
       </a>
       . Coded by <a href="https://github.com/PriyoBROTO04">Priyobroto Kar</a>.
-    </div>
+    </footer>
   );
 };
 
