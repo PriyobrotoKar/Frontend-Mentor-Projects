e48422017a1bf74d39c0af89c5f776dbf63dec21
Priyobroto Kar <priyobrotokar@gmail.com>
2023-05-23 20:03:19 +0530

    Fixed accessibility

--- Project 14/src/BannerImg.jsx
+++ Project 14/src/BannerImg.jsx
@@ -2,13 +2,14 @@ import React from "react";
 
 const BannerImg = () => {
   return (
-    <div className="lg:rounded-l-xl overflow-hidden">
+    <section className="lg:rounded-l-xl overflow-hidden">
+      <h2 className="hidden">Article Image</h2>
       <img
         className="h-[13.2rem] lg:h-full w-full object-cover object-top lg:object-[20%_100%]"
         src="./images/drawers.jpg"
         alt=""
       />
-    </div>
+    </section>
   );
 };
 
