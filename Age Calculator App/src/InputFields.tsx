d3dd1dc45154ae162dc5eb7d615c332e080aec31
Priyobroto Kar <priyobrotokar@gmail.com>
2023-07-11 23:25:57 +0530

    Added the submit check feature

--- Age Calculator App/src/InputFields.tsx
+++ Age Calculator App/src/InputFields.tsx
@@ -1,50 +1,103 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+type InputType = {
+  day: number | null;
+  month: number | null;
+  year: number | null;
+};
 
 const InputFields = () => {
+  const [input, setInput] = useState<InputType>({
+    day: null,
+    month: null,
+    year: null,
+  });
+  const [error, setError] = useState({
+    day: { status: false, msg: "" },
+    month: { status: false, msg: "" },
+    year: { status: false, msg: "" },
+  });
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInput({ ...input, [e.target.name]: e.target.value });
+  };
+  const checkIsEmpty = (key: keyof typeof input) => {
+    if (!input[key]) {
+      setError((prev) => ({
+        ...prev,
+        [key]: { status: true, msg: "This field is required" },
+      }));
+    } else {
+      setError((prev) => ({
+        ...prev,
+        [key]: { status: false, msg: "" },
+      }));
+    }
+  };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    console.log("submitt");
+    if (input?.day && input?.month && input?.year) {
+      console.log("submitt");
+    }
+    let key: keyof typeof input;
+    for (key in input) {
+      checkIsEmpty(key);
+    }
   };
+
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex justify-between gap-3">
         <div>
           <label
-            htmlFor="name"
-            className="text-xs text-Smokey-grey font-bold tracking-widest"
+            htmlFor="day"
+            className={
+              "text-xs  font-bold tracking-widest " +
+              (error.day.status ? "text-Light-red" : "text-Smokey-grey")
+            }
           >
             DAY
           </label>
           <input
             className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
-            id="name"
+            id="day"
+            name="day"
             type="number"
+            onChange={handleInput}
           />
         </div>
         <div>
           <label
             htmlFor="month"
-            className="text-xs text-Smokey-grey font-bold tracking-widest"
+            className={
+              "text-xs text-Smokey-grey font-bold tracking-widest " +
+              (error.month.status ? "text-Light-red" : "text-Smokey-grey")
+            }
           >
             MONTH
           </label>
           <input
             className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
             id="month"
+            name="month"
             type="number"
+            onChange={handleInput}
           />
         </div>
         <div>
           <label
             htmlFor="year"
-            className="text-xs text-Smokey-grey font-bold tracking-widest"
+            className={
+              "text-xs text-Smokey-grey font-bold tracking-widest " +
+              (error.year.status ? "text-Light-red" : "text-Smokey-grey")
+            }
           >
             YEAR
           </label>
           <input
             className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
             id="year"
+            name="year"
             type="number"
+            onChange={handleInput}
           />
         </div>
       </div>
