fcaa0483d47f3e1e88c4f7468bf7fef006399c48
Priyobroto Kar <priyobrotokar@gmail.com>
2023-07-14 20:44:16 +0530

    Added the error msgs

--- Age Calculator App/src/InputFields.tsx
+++ Age Calculator App/src/InputFields.tsx
@@ -42,11 +42,35 @@ const InputFields = () => {
       checkIsEmpty(key);
     }
   };
+  useEffect(() => {
+    if (input.day && input.day > 31) {
+      setError((prev) => ({
+        ...prev,
+        day: { status: true, msg: "Must be a valid date" },
+      }));
+    } else {
+      setError((prev) => ({
+        ...prev,
+        day: { status: false, msg: "" },
+      }));
+    }
+    if (input.month && input.month > 12) {
+      setError((prev) => ({
+        ...prev,
+        month: { status: true, msg: "Must be a valid month" },
+      }));
+    } else {
+      setError((prev) => ({
+        ...prev,
+        month: { status: false, msg: "" },
+      }));
+    }
+  }, [input]);
 
   return (
     <form onSubmit={handleSubmit}>
       <div className="flex justify-between gap-3">
-        <div>
+        <div className="relative">
           <label
             htmlFor="day"
             className={
@@ -57,48 +81,66 @@ const InputFields = () => {
             DAY
           </label>
           <input
-            className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
+            className={
+              "w-full border  rounded-lg py-2.5 px-3 text-xl font-bold " +
+              (error.day.status ? "border-Light-red" : "border-Light-grey")
+            }
             id="day"
             name="day"
             type="number"
             onChange={handleInput}
           />
+          <div className="absolute top-full text-xs mt-1 text-Light-red italic">
+            {error.day.msg}
+          </div>
         </div>
-        <div>
+        <div className="relative">
           <label
             htmlFor="month"
             className={
-              "text-xs text-Smokey-grey font-bold tracking-widest " +
+              "text-xs  font-bold tracking-widest " +
               (error.month.status ? "text-Light-red" : "text-Smokey-grey")
             }
           >
             MONTH
           </label>
           <input
-            className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
+            className={
+              "w-full border  rounded-lg py-2.5 px-3 text-xl font-bold " +
+              (error.month.status ? "border-Light-red" : "border-Light-grey")
+            }
             id="month"
             name="month"
             type="number"
             onChange={handleInput}
           />
+          <div className="absolute top-full text-xs mt-1 text-Light-red italic">
+            {error.month.msg}
+          </div>
         </div>
-        <div>
+        <div className="relative">
           <label
             htmlFor="year"
             className={
-              "text-xs text-Smokey-grey font-bold tracking-widest " +
+              "text-xs  font-bold tracking-widest " +
               (error.year.status ? "text-Light-red" : "text-Smokey-grey")
             }
           >
             YEAR
           </label>
           <input
-            className="w-full border border-Light-grey rounded-lg py-2.5 px-3 text-xl font-bold"
+            className={
+              "w-full border  rounded-lg py-2.5 px-3 text-xl font-bold " +
+              (error.year.status ? "border-Light-red" : "border-Light-grey")
+            }
             id="year"
             name="year"
             type="number"
             onChange={handleInput}
           />
+          <div className="absolute top-full text-xs mt-1 text-Light-red italic">
+            {error.year.msg}
+          </div>
         </div>
       </div>
       <div className="flex justify-center items-center flex-col relative py-7">
