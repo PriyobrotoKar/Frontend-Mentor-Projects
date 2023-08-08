a6b1c177b4e447b8a45dd8c6487e803249f25395
Priyobroto Kar <priyobrotokar@gmail.com>
2023-08-08 19:08:19 +0530

    Ini

--- Notifications Page Main/src/App.tsx
+++ Notifications Page Main/src/App.tsx
@@ -1,5 +1,24 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <div>
+      Notifications 3 Mark all as read Mark Webber reacted to your recent post
+      My first tournament today! 1m ago Angela Gray followed you 5m ago Jacob
+      Thompson has joined your group Chess Club 1 day ago Rizky Hasanuddin sent
+      you a private message 5 days ago Hello, thanks for setting up the Chess
+      Club. I've been a member for a few weeks now and I'm already having lots
+      of fun and improving my game. Kimberly Smith commented on your picture 1
+      week ago Nathan Peterson reacted to your recent post 5 end-game strategies
+      to increase your win rate 2 weeks ago Anna Kim left the group Chess Club 2
+      weeks ago
+      <div className="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Your Name Here</a>.
+      </div>
+    </div>
+  );
 }
 
 export default App;
